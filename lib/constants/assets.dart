36961e86824ca58b7d920d47a69ff1540cc247a9
Tauqeer-flutter <tauqeer.flutter@tafsol.com>
2026-05-05 23:27:48 +0500

    Add map section and location card to home page

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -4,7 +4,8 @@ abstract final class SvgAssets {
 }
 
 class DummyAssets {
-  //  static const String _assets = 'assets/dummy/';
+  static const String _assets = 'assets/dummy';
+  static const String smallMap = '$_assets/small_map.png';
 }
 
 class PngAssets {
