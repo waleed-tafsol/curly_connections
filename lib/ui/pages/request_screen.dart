bff82a81f5013bb9efddf329495e5523f82e8843
saboor-flutter <saboor.flutter@tafsol.com>
2026-05-07 12:24:12 +0500

    feat: Update RequestScreen to handle user types and display appropriate content

--- lib/ui/pages/request_screen.dart
+++ lib/ui/pages/request_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 import 'package:tabler_icons_plus/tabler_icons_plus.dart';
 
 import '../../utils/enums.dart';
 import '../resources/app_colors.dart';
 import '../resources/app_fonts.dart';
+import '../view_models/auth_view_model.dart';
+import '../widgets/booking_request_card.dart';
 import '../widgets/bottom sheet/filter_sheet.dart';
 import '../widgets/service_request_card.dart';
 
@@ -20,9 +23,11 @@ class _RequestScreenState extends State<RequestScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final userType = context.read<AuthViewModel>().userType;
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 18.w),
       child: Column(
+        crossAxisAlignment: .start,
         children: [
           ListTile(
             title: Text("Service requests", style: AppFonts.black20w400),
@@ -32,37 +37,50 @@ class _RequestScreenState extends State<RequestScreen> {
             ),
           ),
           SizedBox(height: 22.h),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: List.generate(ServiceRequest.values.length, (index) {
-                final isSelected = _selectedIndex == index;
-                return Padding(
-                  padding: EdgeInsets.only(right: 12.w),
-                  child: GestureDetector(
-                    onTap: () => setState(() => _selectedIndex = index),
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 20.w,
-                        vertical: 12.h,
-                      ),
-                      decoration: BoxDecoration(
-                        color: isSelected
-                            ? Colors.white.withValues(alpha: 0.4)
-                            : Colors.transparent,
-                        borderRadius: BorderRadius.circular(50.r),
-                        border: Border.all(color: Colors.white),
-                      ),
-                      child: Text(
-                        ServiceRequest.values[index].name,
-                        style: AppFonts.black14w400,
+          if (userType != UserType.client)
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(ServiceRequest.values.length, (index) {
+                  final isSelected = _selectedIndex == index;
+                  return Padding(
+                    padding: EdgeInsets.only(right: 12.w),
+                    child: GestureDetector(
+                      onTap: () => setState(() => _selectedIndex = index),
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 20.w,
+                          vertical: 12.h,
+                        ),
+                        decoration: BoxDecoration(
+                          color: isSelected
+                              ? Colors.white.withValues(alpha: 0.4)
+                              : Colors.transparent,
+                          borderRadius: BorderRadius.circular(50.r),
+                          border: Border.all(color: Colors.white),
+                        ),
+                        child: Text(
+                          ServiceRequest.values[index].name,
+                          style: AppFonts.black14w400,
+                        ),
                       ),
                     ),
-                  ),
-                );
-              }),
+                  );
+                }),
+              ),
+            )
+          else
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 12.h),
+              decoration: BoxDecoration(
+                color: Colors.white.withValues(alpha: 0.4),
+
+                borderRadius: BorderRadius.circular(50.r),
+                border: Border.all(color: Colors.white),
+              ),
+              child: Text("Booking", style: AppFonts.black14w400),
             ),
-          ),
+
           SizedBox(height: 18.h),
           TextField(
             decoration: InputDecoration(
@@ -98,32 +116,21 @@ class _RequestScreenState extends State<RequestScreen> {
                 builder: (_) => const FilterSheet(),
               );
             },
-            child: GestureDetector(
-              onTap: () {
-                showModalBottomSheet(
-                  context: context,
-                  isScrollControlled: true,
-                  backgroundColor: Colors.transparent,
-                  barrierColor: Colors.white.withValues(alpha: 0.5),
-                  builder: (_) => const FilterSheet(),
-                );
-              },
-              child: Row(
-                mainAxisAlignment: .end,
-                children: [
-                  Text("Filter", style: AppFonts.black14w500),
-                  SizedBox(width: 5.w),
-                  Icon(TablerIcons.filter, color: AppColors.black, size: 23.sp),
-                  SizedBox(width: 20.w),
-                  Text("Sort By", style: AppFonts.black14w500),
-                  SizedBox(width: 5.w),
-                  Icon(
-                    TablerIcons.sortDescending,
-                    color: AppColors.black,
-                    size: 23.sp,
-                  ),
-                ],
-              ),
+            child: Row(
+              mainAxisAlignment: .end,
+              children: [
+                Text("Filter", style: AppFonts.black14w500),
+                SizedBox(width: 5.w),
+                Icon(TablerIcons.filter, color: AppColors.black, size: 23.sp),
+                SizedBox(width: 20.w),
+                Text("Sort By", style: AppFonts.black14w500),
+                SizedBox(width: 5.w),
+                Icon(
+                  TablerIcons.sortDescending,
+                  color: AppColors.black,
+                  size: 23.sp,
+                ),
+              ],
             ),
           ),
           SizedBox(height: 16.h),
@@ -140,7 +147,9 @@ class _RequestScreenState extends State<RequestScreen> {
                         ? MediaQuery.paddingOf(context).bottom + 30.h
                         : 15.h,
                   ),
-                  child: ServiceRequestCard(index: _selectedIndex),
+                  child: userType == UserType.client
+                      ? const BookingRequestCard()
+                      : ServiceRequestCard(index: _selectedIndex),
                 );
               },
             ),
